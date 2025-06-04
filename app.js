fe243145acb6bc8105e0604a3d62ef2657afca68
davidmx15 <mlpdmx12@gmail.com>
2025-06-03 22:11:26 -0600

    app.js

--- app.js
+++ app.js
@@ -110,33 +110,3 @@ app.get('/corte/ticket', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-// Middleware para poder leer datos del body
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-const PORT = process.env.PORT || 3000;
-
-// Configuración del motor de vistas
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Middleware para servir archivos estáticos (como imágenes, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Rutas
-app.get('/', (req, res) => {
-    res.render('Inicio');
-});
-
-// Arrancar servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
